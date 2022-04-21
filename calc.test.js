3fc96ad010e38a0c1950211e7cfa619f279a6577
Vardges Hamazaspyan <vardges.hamazaspyan@dataart.com>
2022-04-21 13:10:39 +0400

    commit

---
+++ calc.test.js
@@ -0,0 +1,5 @@
+import { calculate } from "./src/App";
+
+test('should ', function () {
+    expect(calculate(2,3)).toBe(5)
+}); 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+export const calculate = (a,b) => {
+  return a + b
+}
+
 function App() {
   return (
     <div className="App">
